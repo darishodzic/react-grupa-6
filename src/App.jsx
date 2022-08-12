55406e9ae5e20da0dbcb2406e86000f9d1554496
daris <daris.hodzic.dh@gmail.com>
2022-08-12 20:49:03 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,7 @@
-import { Route, Routes, Link } from "react-router-dom";
-import Counter from "./components/Counter";
-import Dropdown from "./components/Dropdown";
 import Todo from "./components/Todo";
 
 const App = () => {
-  return (
-    <div>
-      <Routes>
-        <Route path={"/counter"} element={<Counter />} />
-        <Route path={"/dropdown"} element={<Dropdown />} />
-        <Route path={"/list"} element={<Todo />} />
-        <Route path={"/"} element={<h1>WELCOME</h1>} />
-      </Routes>
-      <div>
-        <Link to={"/"}>
-          <h1>Welcome</h1>
-        </Link>
-        <Link to={"/counter"}>
-          <h1>Counter</h1>
-        </Link>
-        <Link to={"/dropdown"}>
-          <h1>Dropdown</h1>
-        </Link>
-        <Link to={"/list"}>
-          <h1>List</h1>
-        </Link>
-      </div>
-    </div>
-  );
+  return <Todo />;
 };
 
 export default App;
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,10 +1,23 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
 import { Link } from "react-router-dom";
 import Card from "./Card";
-const Todo = () => {
-  const [data, setData] = useState([]);
 
-  let a = "";
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "ADD":
+      return [...state, action.payload];
+    case "DELETE":
+      return state.filter((el, index) => index !== action.payload);
+    default:
+      return state;
+  }
+};
+
+const Todo = () => {
+  // const [data, setData] = useState([]);
+  const [data, dispatch] = useReducer(reducer, []);
+  const [input, setInput] = useState("");
+
   return (
     <div className="container">
       <h1>TODO LIST</h1>
@@ -13,14 +26,16 @@ const Todo = () => {
         className="unos"
         onSubmit={(e) => {
           e.preventDefault();
-          setData([...data, a]);
+          dispatch({ type: "ADD", payload: input });
+          setInput("");
         }}
       >
         <input
           type="text"
           onChange={(e) => {
-            a = e.target.value;
+            setInput(e.target.value);
           }}
+          value={input}
         />
         <button type="submit">Submit</button>
       </form>
@@ -29,7 +44,7 @@ const Todo = () => {
           <Card
             title={el}
             onDelete={() => {
-              setData(data.filter((el, index) => i !== index));
+              dispatch({ type: "DELETE", payload: i });
             }}
           />
         );
