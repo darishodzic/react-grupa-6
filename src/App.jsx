8ba00e0f48b385d9427e2331cf25de9b4eff79d9
darishodzic <daris.hodzic.dh@gmail.com>
2022-08-24 20:57:10 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
-import Todo from "./components/Todo";
-
+import { useContext } from "react";
+import { Context } from "./UserContext";
 const App = () => {
-  return <Todo />;
+  const { user, setUser } = useContext(Context);
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setUser(user - 1);
+        }}
+      >
+        -
+      </button>
+      <h1>{user}</h1>
+      <button
+        onClick={() => {
+          setUser(user + 1);
+        }}
+      >
+        +
+      </button>
+    </div>
+  );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import UserContext from "./UserContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <BrowserRouter>
-    <App />
+    <UserContext>
+      <App />
+    </UserContext>
   </BrowserRouter>
 );
